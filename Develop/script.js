02f8a99c4ec6242248c68d1484696ccfddc52501
Annie Hoang <anniehoang.anh@gmail.com>
2022-06-08 23:17:02 -0400

    timeblock colors

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,27 @@
-var currentTime = moment().hour()
+var timeBlock=[9,10,11,12,13,14,15,16,17]
+var currentHour = moment().hour()
+
 var currentDay=$("#currentDay")
+console.log(currentHour)
 
 var systemTime=moment().format("dddd, MMMM Do")
 currentDay.text(systemTime)
 
+function timeBlockColor(){
+    for (var i=0; i < timeBlock.length; i++) {
+        var currentTextEl = $("#" + timeBlock[i])
 
-console.log(currentTime)
+        if (currentHour > timeBlock[i]){
+            currentTextEl.addClass("past")
+        }
+        else if(currentHour === timeBlock[i]){
+            currentTextEl.addClass("present")
+        }
+        else {
+            currentTextEl.addClass("future")
+        }
+        
+    }
+}
+
+timeBlockColor()
