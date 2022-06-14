da4371502024f40fe27b5a8bc3611f0ce0fa58a4
Annie Hoang <anniehoang.anh@gmail.com>
2022-06-14 16:51:03 -0400

    local storage

--- Develop/script.js
+++ Develop/script.js
@@ -25,3 +25,25 @@ function timeBlockColor(){
 }
 
 timeBlockColor()
+
+
+
+var saveBtn = $(".saveBtn")
+saveBtn.on('click', function () {
+
+    const textValue = $(this).siblings(".textArea").val();
+    const timeBlock = $(this).parent().attr('id')
+    console.log("TimeBlock", timeBlock ,"text Val", textValue)
+    localStorage.setItem(timeBlock, textValue)
+  });
+
+
+$(`#9b .textArea`).val(localStorage.getItem('9b'))
+$(`#10b .textArea`).val(localStorage.getItem('10b'))
+$(`#11b .textArea`).val(localStorage.getItem('11b'))
+$(`#12b .textArea`).val(localStorage.getItem('12b'))
+$(`#13b .textArea`).val(localStorage.getItem('13b'))
+$(`#14b .textArea`).val(localStorage.getItem('14b'))
+$(`#15b .textArea`).val(localStorage.getItem('15b'))
+$(`#16b .textArea`).val(localStorage.getItem('16b'))
+$(`#17b .textArea`).val(localStorage.getItem('17b'))
